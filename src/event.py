34cb6b681ea8c39c173d0909dcf818671b05f6e9
yuqi fu <fuyuqi1995@gmail.com>
2024-12-17 18:33:42 -0500

    add event

--- src/event.py
+++ src/event.py
@@ -3,9 +3,8 @@ import time
 
 class Event(ABC):
     """Abstract base class for all events."""
-    def __init__(self, event_type, timestamp=None):
+    def __init__(self, event_type):
         self.event_type = event_type
-        self.timestamp = 0  # 默认为当前时间戳
 
     @abstractmethod
     def __repr__(self):
@@ -14,8 +13,8 @@ class Event(ABC):
 
 class AddFileEvent(Event):
     """Event for adding a file to a node."""
-    def __init__(self, node, file_name, timestamp=None):
-        super().__init__(event_type="ADD_FILE", timestamp=timestamp)
+    def __init__(self, node, file_name, timestamp):
+        super().__init__(event_type="ADD_FILE")
         self.node = node
         self.file_name = file_name
         self.timestamp = timestamp
@@ -25,19 +24,21 @@ class AddFileEvent(Event):
 
 class GetFileEvent(Event):
     """Event for retrieving a file from the network."""
-    def __init__(self, node, cid, timestamp=None):
-        super().__init__(event_type="GET_FILE", timestamp=timestamp)
+    def __init__(self, node, cid, timestamp):
+        super().__init__(event_type="GET_FILE")
         self.node = node
         self.cid = cid
+        self.timestamp = timestamp
 
     def __repr__(self):
         return f"[{self.timestamp}] GET_FILE: CID '{self.cid}' fetched at Node {self.node}"
     
 class LargeScaleFailureEvent(Event):
     """Event for simulating a large-scale node failure."""
-    def __init__(self, failure_nodes, timestamp=None):
-        super().__init__(event_type="FAILURE", timestamp=timestamp)
+    def __init__(self, failure_nodes, timestamp):
+        super().__init__(event_type="FAILURE")
         self.failure_nodes = failure_nodes
+        self.timestamp = timestamp
 
     def __repr__(self):
         return f"[{self.timestamp}] FAILURE: Nodes {self.failure_nodes} failed"
