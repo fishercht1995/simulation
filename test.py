a41c8894b1a4dddb9f6206edb1285db11a37554f
yuqi fu <fuyuqi1995@gmail.com>
2024-12-17 15:38:06 -0500

    add test

--- test.py
+++ test.py
@@ -9,20 +9,19 @@ sys.path.append(src_dir)
 # 导入 IPTB 类
 from iptb import IPTB
 from ipfs import IPFSCluster
-
+import time
 def main():
     # 测试 IPTB 类
-    """
+
     print("IPTB 0")
     iptb = IPTB(num_nodes=5, base_path="./testbed", node_type="localipfs")
     print("IPTB 1")
     iptb.init_network()
     iptb.start()
     print("IPTB network setup tested successfully!")
-    """
+    time.sleep(20)
     ipfs = IPFSCluster()
     ipfs.connect(1,4)
-    ipfs.connect(4,1)
     print("add")
     cid = ipfs.add_file(4, "~/test")
     print(f"Get {cid}")
